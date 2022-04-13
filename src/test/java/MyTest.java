ffb3e5b04f21a8306b3c3bf82762477b735e8820
epikover <epikover@digibank.co.il>
2022-04-13 20:57:53 +0300

    just a draft

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyTest {
+    int x = 5;
+    int y = 5;
+    @Test
+    public void test_a(){
+        Assert.assertEquals(x,y);
+    }
+}
